445dfc807878fe4182ab65fbdb1ea263cd793e34
shop <tungxn1594@gmail.com>
2017-04-21 11:55:45 +0700

    migration and adding css/js files

--- database/migrations/2017_04_20_101108_create_members_table.php
+++ database/migrations/2017_04_20_101108_create_members_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMembersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('photo')->nullable()->default('default.png');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 100);
+            $table->string('address', 300);
+            $table->tinyInteger('age');
+            $table->tinyInteger('gender')->comment('0 : Male, 1 : Female');
+            $table->string('photo');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('members');
     }
 }
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin-layout')
+
+@section('contents')
+	<div class="col-md-12">
+		abc
+	</div>
+@endsection
